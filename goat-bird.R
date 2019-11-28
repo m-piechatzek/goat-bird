68ebd19a015d9d893b8e0d907148096eaa601fe7
MonikaP <piechatzek@hotmail.com>
2019-11-27 20:51:47 -0800

    Added some mean and median and filtering for specific bird

--- goat-bird.R
+++ goat-bird.R
@@ -14,4 +14,27 @@ a
 # shows how many votes per hour in a bar graph
 ggplot(nz_bird, aes(hour)) + geom_bar()
 
+# Count of all votes of birds
+ggplot(nz_bird, aes(bird_breed)) + geom_bar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# Count of Westland Petrel votes per hour
+nz_bird %>% filter(nz_bird$bird_breed=="Westland Petrel") %>% ggplot(nz_bird, mapping= aes(hour)) + geom_bar()
+
+# Testing with Westland Petrel votes per hour
+test <- filter(nz_bird, bird_breed=="Westland Petrel")
+attach(test)
+test[order(hour),]
+
+
+
 # Add in means, median, mode
+
+# Mean of hours in histogram
+ggplot(nz_bird, aes(hour)) + geom_histogram(color="black", fill="white", binwidth = 1) + geom_vline(aes(xintercept=mean(hour)),color="blue", linetype="dashed", size=1)
+
+# Median of hours in histogram
+ggplot(nz_bird, aes(hour)) + geom_histogram(color="black", fill="white", binwidth = 1) + geom_vline(aes(xintercept=median(hour)),color="blue", linetype="dashed", size=1)
+
+#
+
+
